4e125a38ce30814478538b6e0c57122518d96f19
Sajjad Ali <59386007+SajjadAliKhojaye@users.noreply.github.com>
2023-12-04 16:12:33 +0500

    Update second_streamlit_app.py
    Added Menu

--- second_streamlit_app.py
+++ second_streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("My Parents' New Healthy Diner")
+
+st.header("Breakfast Menu")
+
+st.text("Omega 3 and Blueberry Oatmeal")
+st.text("Kale, Spanish and Rocket Smoothie")
+st.text("Hard-Boiled Free-Range Egg")
